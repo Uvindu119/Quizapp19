bc8add1657b419f0196bc7cdbfe785b88ea627d3
Uvindu119 <trolllol88@gmail.com>
2023-03-30 10:34:56 +0530

    its building yet

--- app/src/main/java/com/example/quizapp19/ResultsActivity.kt
+++ app/src/main/java/com/example/quizapp19/ResultsActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 
 class ResultsActivity : AppCompatActivity() {
     private lateinit var scoreTextView: TextView
+    private lateinit var gradeTextView: TextView
     private lateinit var incorrectAnswersTextView: TextView
     private lateinit var restartButton: Button
     private lateinit var exitButton: Button
@@ -17,6 +18,7 @@ class ResultsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_results)
 
         scoreTextView = findViewById(R.id.scoreTextView)
+        gradeTextView = findViewById(R.id.gradeTextView)
         incorrectAnswersTextView = findViewById(R.id.incorrectAnswersTextView)
         restartButton = findViewById(R.id.restartButton)
         exitButton = findViewById(R.id.exitButton)
@@ -25,7 +27,26 @@ class ResultsActivity : AppCompatActivity() {
         val totalQuestions = intent.getIntExtra("totalQuestions", 0)
         val incorrectAnswers = intent.getStringArrayListExtra("incorrectAnswers") ?: arrayListOf()
 
-        scoreTextView.text = getString(R.string.score_text, correctAnswers, totalQuestions)
+        val score = correctAnswers * 2
+        scoreTextView.text = getString(R.string.score_text, score, totalQuestions)
+
+        if (score > 16) {
+            gradeTextView.text = "A"
+            gradeTextView.setTextColor(resources.getColor(R.color.green))
+        }
+        else if (score > 12) {
+            gradeTextView.text = "B"
+            gradeTextView.setTextColor(resources.getColor(R.color.light_green))
+        }
+        else if (score > 8) {
+            gradeTextView.text = "C"
+            gradeTextView.setTextColor(resources.getColor(R.color.yellow))
+        }
+        else {
+            gradeTextView.text = "W"
+            gradeTextView.setTextColor(resources.getColor(R.color.red))
+        }
+
         incorrectAnswersTextView.text = getString(R.string.incorrect_answers_text, incorrectAnswers.joinToString("\n\n"))
 
         restartButton.setOnClickListener {
@@ -37,6 +58,5 @@ class ResultsActivity : AppCompatActivity() {
         exitButton.setOnClickListener {
             finish()
         }
-
     }
 }
