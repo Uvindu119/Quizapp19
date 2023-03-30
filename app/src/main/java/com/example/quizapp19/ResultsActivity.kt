22e4b6c5152b626eee6e865aca92a63eed16bc7f
Uvindu119 <trolllol88@gmail.com>
2023-03-30 18:50:30 +0530

    its building yet

--- app/src/main/java/com/example/quizapp19/ResultsActivity.kt
+++ app/src/main/java/com/example/quizapp19/ResultsActivity.kt
@@ -12,6 +12,7 @@ class ResultsActivity : AppCompatActivity() {
     private lateinit var incorrectAnswersTextView: TextView
     private lateinit var restartButton: Button
     private lateinit var exitButton: Button
+    private lateinit var nextPaperButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +23,7 @@ class ResultsActivity : AppCompatActivity() {
         incorrectAnswersTextView = findViewById(R.id.incorrectAnswersTextView)
         restartButton = findViewById(R.id.restartButton)
         exitButton = findViewById(R.id.exitButton)
+        nextPaperButton = findViewById(R.id.nextPaperButton)
 
         val correctAnswers = intent.getIntExtra("correctAnswers", 0)
         val totalQuestions = intent.getIntExtra("totalQuestions", 0)
@@ -30,27 +32,41 @@ class ResultsActivity : AppCompatActivity() {
         val score = correctAnswers*2
         scoreTextView.text = getString(R.string.score_text, score, totalQuestions)
 
-        if (score > 16) {
-            gradeTextView.text = "A"
-            gradeTextView.setTextColor(resources.getColor(R.color.green))
-        }
-        else if (score > 12) {
-            gradeTextView.text = "B"
-            gradeTextView.setTextColor(resources.getColor(R.color.light_green))
-        }
-        else if (score > 8) {
-            gradeTextView.text = "C"
-            gradeTextView.setTextColor(resources.getColor(R.color.yellow))
-        }
-        else {
-            gradeTextView.text = "W"
-            gradeTextView.setTextColor(resources.getColor(R.color.red))
+        val isQualifiedForNextPaper = when (score) {
+            in 17..totalQuestions -> {
+                gradeTextView.text = "A"
+                gradeTextView.setTextColor(resources.getColor(R.color.green))
+                true
+            }
+            in 13..16 -> {
+                gradeTextView.text = "B"
+                gradeTextView.setTextColor(resources.getColor(R.color.light_green))
+                true
+            }
+            in 9..12 -> {
+                gradeTextView.text = "C"
+                gradeTextView.setTextColor(resources.getColor(R.color.yellow))
+                true
+            }
+            else -> {
+                gradeTextView.text = "W"
+                gradeTextView.setTextColor(resources.getColor(R.color.red))
+                false
+            }
         }
 
         incorrectAnswersTextView.text = getString(R.string.incorrect_answers_text, incorrectAnswers.joinToString("\n\n"))
 
+        nextPaperButton.isEnabled = isQualifiedForNextPaper
+        nextPaperButton.setOnClickListener {
+            if (isQualifiedForNextPaper) {
+                val intent = Intent(this, QuizActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
         restartButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, QuizActivity::class.java)
             startActivity(intent)
             finish()
         }
