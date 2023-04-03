48644e845a677933dcca4f0f5868b7de34c1f40b
Uvindu119 <trolllol88@gmail.com>
2023-04-03 21:06:14 +0530

    its building yet

--- app/src/main/java/com/example/quizapp19/QuizDatabaseHelper.kt
+++ app/src/main/java/com/example/quizapp19/QuizDatabaseHelper.kt
@@ -10,7 +10,7 @@ class QuizDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_
 
     companion object {
         private const val DATABASE_NAME = "Quiz.db"
-        private const val DATABASE_VERSION = 36
+        private const val DATABASE_VERSION = 37
 
         private const val TABLE_NAME = "questions"
         private const val COLUMN_ID = "id"
@@ -111,7 +111,7 @@ class QuizDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_
         db?.insert(TABLE_NAME, null, contentValues)
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) { if (oldVersion < 36 && newVersion >= 36) {
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) { if (oldVersion < 37 && newVersion >= 37) {
         // Option 1: Drop the table and recreate it
         db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
         onCreate(db)
